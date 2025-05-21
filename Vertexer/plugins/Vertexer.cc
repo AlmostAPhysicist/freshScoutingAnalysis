49b228630c53fb835c3fc62d5f64904ef77f7675
Bruno Lopes <bruno.lopes@cern.ch>
2025-05-21 19:21:41 +0200

    Fixed typo (opsie)

--- Vertexer/plugins/Vertexer.cc
+++ Vertexer/plugins/Vertexer.cc
@@ -288,7 +288,7 @@ void Vertexer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
     std::pair<bool, Measurement1D> ttk_dist = IPTools::absoluteTransverseImpactParameter(ttk, fake_bs_vtx);
     //std::pair<bool, Measurement1D> ttk_dist = track_dist(ttk, fake_bs_vtx);
     float IP_sig = ttk_dist.second.significance();
-    if ((IP_sig > dxySig_min_cut) && (IP_sig > dxySig_max_cut)  && (tk_ref->pt()>pt_min_cut) && (tk_ref->hitPattern().numberOfValidPixelHits() > npixelHits_min_cut) && (tk_ref->hitPattern().trackerLayersWithMeasurement() > ntrackerLayers_min_cut)){
+    if ((IP_sig > dxySig_min_cut) && (IP_sig < dxySig_max_cut)  && (tk_ref->pt()>pt_min_cut) && (tk_ref->hitPattern().numberOfValidPixelHits() > npixelHits_min_cut) && (tk_ref->hitPattern().trackerLayersWithMeasurement() > ntrackerLayers_min_cut)){
       seed_track_refs.push_back(tk_ref);
       seed_track_index_map[tk_ref] = i_tk;
     }
